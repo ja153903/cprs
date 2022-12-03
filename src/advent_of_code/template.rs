f0d6ec1c0f044457a40b36b9eed4b116b71dfc02
ja153903 <jaimeabbariao@gmail.com>
2022-12-03 13:40:53 -0500

    feat: adding template for advent of code

---
+++ src/advent_of_code/template.rs
@@ -0,0 +1,54 @@
+#![allow(dead_code)]
+
+use std::io::{self};
+use crate::advent_of_code::helpers::file::read_lines;
+
+fn part1(path: &str) -> io::Result<i32> {
+    match read_lines(path) {
+        Err(e) => Err(e),
+        Ok(lines) => Ok(0),
+    }
+}
+
+fn part2(path: &str) -> io::Result<i32> {
+    match read_lines(path) {
+        Err(e) => Err(e),
+        Ok(lines) => Ok(0),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+
+    const SAMPLE_DATA: &str = "./src/advent_of_code/<>/data/<>";
+    const DATA: &str = "./src/advent_of_code/<>/data/<>";
+
+    #[ignore = "no implementation"]
+    #[test]
+    pub fn run_part1_sample() {
+        let result = part1(SAMPLE_DATA);
+        assert_eq!(result.unwrap(), 0);
+    }
+
+    #[ignore = "no implementation"]
+    #[test]
+    pub fn run_part1() {
+        let result = part1(DATA);
+        assert_eq!(result.unwrap(), 0);
+    }
+
+    #[ignore = "no implementation"]
+    #[test]
+    pub fn run_part2_sample() {
+        let result = part2(SAMPLE_DATA);
+        assert_eq!(result.unwrap(), 0);
+    }
+
+    #[ignore = "no implementation"]
+    #[test]
+    pub fn run_part2() {
+        let result = part2(DATA);
+        assert_eq!(result.unwrap(), 0);
+    }
+}
